a6c0ca2bc4cee802a08fddcc7edd17560c0741fd
unknown <miyashaheen1995@gmail.com>
2020-11-04 19:14:30 +0530

    Modified:functionalities

--- sort_arithmetic_computation.sh
+++ sort_arithmetic_computation.sh
@@ -4,7 +4,11 @@ echo "Arithmetic Computation and Sorting!"
 read -p "Enter a : " a
 read -p "Enter b : " b
 read -p "Enter c : " c
-(( a+b*c ))
-(( a*b+c ))
-(( c+a/b ))
-(( a%b+c ))
+
+#declare a dictionary
+declare -A dictCompute
+#store the results in dictionary for every computation
+dictCompute[expression1]=$(( a+b*c ))
+dictCompute[expression2]=$(( a*b+c ))
+dictCompute[expression3]=$(( c+a/b ))
+dictCompute[expression4]=$(( a%b+c ))
