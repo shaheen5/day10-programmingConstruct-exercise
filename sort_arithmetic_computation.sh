b0f9fc34e3656249e447819adb8f96d73c9dc89d
unknown <miyashaheen1995@gmail.com>
2020-11-05 18:13:04 +0530

    Added:functionalities

--- sort_arithmetic_computation.sh
+++ sort_arithmetic_computation.sh
@@ -7,6 +7,7 @@ read -p "Enter c : " c
 
 #declare a dictionary
 declare -A dictCompute
+
 #store the results in dictionary for every computation
 dictCompute[expression1]=$(( a+b*c ))
 dictCompute[expression2]=$(( a*b+c ))
@@ -19,3 +20,33 @@ do
 	arr[count++]=$value
 done
 
+#display elements of array
+echo -e "Computation Result in Array:\n"${arr[@]}
+
+#bubble_sort used to sort array
+function sort_array(){
+
+for ((i=0;i<4;i++))
+do
+	for (( j=0;j<4-i-1;j++ ))
+	do
+		if [ ${arr[j]} -gt ${arr[$((j+1))]} ]
+		then
+			#swap
+			temp=${arr[j]}
+			arr[$j]=${arr[$((j+1))]}
+			arr[$((j+1))]=$temp
+		fi
+	done
+done
+}
+
+#calling sort function
+sort_array ${arr[@]}
+
+#displaying result in desc order
+echo "Computation Result in Descending Order:"
+for (( i=3;i>=0;i-- ))
+do
+	echo -n "${arr[$i]}  "
+done
